42862d57cd59d11ea034cda5a691d0674b98db8e
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-10-08 18:30:46 +0800

    TST: adjust test case

--- tensorflow/python/kernel_tests/variable_scope_test.py
+++ tensorflow/python/kernel_tests/variable_scope_test.py
@@ -368,11 +368,11 @@ class VariableScopeTest(test.TestCase):
         with variable_scope.variable_scope(
             tower):  # Re-entering acts like another "tower".
           with ops.name_scope("scope2") as sc2:
-            self.assertEqual(sc2, "testVarScopeNameScope1/tower_1/scope2/")
+            self.assertEqual(sc2, "testVarScopeNameScope1/tower/scope2_1/")
         with variable_scope.variable_scope(
             "tower"):  # Re-entering by string acts the same.
           with ops.name_scope("scope2") as sc2:
-            self.assertEqual(sc2, "testVarScopeNameScope1/tower_2/scope2/")
+            self.assertEqual(sc2, "testVarScopeNameScope1/tower_1/scope2/")
 
     with ops.name_scope("testVarScopeNameScope2"):
       with variable_scope.variable_scope("tower"):
